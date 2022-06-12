94fe6b17c18d2216cf15e9e2b31a18ea6d2025d8
Adryel Simon <adryel@live.it>
2022-06-12 18:05:29 -0300

    feedcontainer added

---
+++ src/Components/FeedContainer/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from "./styled";
+
+// import { Container } from './styles';
+
+const FeedContainer =  ({children}:{children: React.ReactNode}) => (
+    <S.FeedContainer>
+        {children}
+    </S.FeedContainer>
+  )
+
+export default FeedContainer;
