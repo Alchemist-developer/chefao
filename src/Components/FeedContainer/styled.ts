160799dc4207957185f4b75be272f493d835cad6
Adryel Simon <adryel@live.it>
2022-06-12 18:04:55 -0300

    feedcontainer added

---
+++ src/Components/FeedContainer/styled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export const FeedContainer = styled.div`
+width: 66%;
+height: 100%;
+display: flex;
+flex-direction: column
+justify-content: center;
+align-items: center;
+`
+
