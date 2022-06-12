fda634ed32b9bbebd262d0670fe29760d35f9263
Thais Cavalcante <thaispcavalcante@gmail.com>
2022-06-12 16:48:29 -0300

    Arquivo Rotas

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,11 +1,10 @@
-import { Routes as WrapperRoutes, Route, BrowserRouter } from 'react-router-dom';
-
+import {Routes as WrapperRoutes, Route, BrowserRouter} from "react-router-dom";
 import PublicFeed from './Pages/PublicFeed';
 
 function Routes(): JSX.Element {
     return (
         <BrowserRouter>
-        <WrapperRoutes>
+            <WrapperRoutes>
             <Route path='/' element={<PublicFeed />}></Route>
         </WrapperRoutes>
         </BrowserRouter>
