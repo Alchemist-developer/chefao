61b06e7615f7eccefa13a0eb3b084948f0a39efb
Adryel Simon <adryel@live.it>
2022-06-12 17:21:09 -0300

    Componentes em páginas de navegação

---
+++ src/Pages/ProfileUser/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from '../../components/Navbar';
+
+
+const ProfileUser: React.FC = () => {
+  return (
+    <Navbar/>
+  );
+}
+
+export default ProfileUser;
