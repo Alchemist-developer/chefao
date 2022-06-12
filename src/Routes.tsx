250ddf4a3690c681f25be92abb1f8f685af2005e
Adryel Simon <adryel@live.it>
2022-06-12 17:21:22 -0300

    Rotas adicionadas

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,11 +1,17 @@
 import {Routes as WrapperRoutes, Route, BrowserRouter} from "react-router-dom";
-import PublicFeed from './Pages/PublicFeed';
+import Login from "./pages/Login";
+import ProfileUser from "./pages/ProfileUser";
+import PublicFeed from './pages/PublicFeed';
+import Register from "./pages/Register";
 
 function Routes(): JSX.Element {
     return (
         <BrowserRouter>
             <WrapperRoutes>
-            <Route path='/' element={<PublicFeed />}></Route>
+            <Route path='/' element={<Login />}></Route>
+            <Route path='/register' element={<Register />}></Route>
+            <Route path='/feed' element={<PublicFeed />}></Route>
+            <Route path='/profile' element={<ProfileUser />}></Route>
         </WrapperRoutes>
         </BrowserRouter>
     )
