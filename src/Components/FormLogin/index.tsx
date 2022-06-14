cf304af66fbc99fd1c3e5a2084520688eb32978c
Thais Cavalcante <thaispcavalcante@gmail.com>
2022-06-14 10:40:38 -0300

    teste

---
+++ src/Components/FormLogin/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FormLogin: React.FC = () => {
+  return (
+    <div className="background">
+
+    </div>
+  );
+}
+
+export default FormLogin;
--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -4,7 +4,7 @@ import Navbar from '../../components/Navbar';
 
 const Login: React.FC = () => {
   return (
-    <Navbar/>
+    <FormLogin />
   );
 }
 
