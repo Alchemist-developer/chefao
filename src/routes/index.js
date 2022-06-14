1c9b0bf6f677d88de813e7bd59c1f4c2ac517084
Guilherme ZB <guiz.48@gmail.com>
2022-06-14 06:16:30 -0300

    Criação das Rotas CRUD

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,22 @@
 const express = require ('express')
-const postController = require ('../domain/Posts/controllers/postController')
-const userController = require ('../domain/Users/controllers/userController')
+const postsController = require ('../domain/Posts/controllers/postsController')
+const usersController = require ('../domain/Users/controllers/usersController')
 const routes = express.Router()
 
-//Rotas CRUD Post
-
-
+//Rotas CRUD Posts
+routes.post('/posts', postsController.createPosts)
+routes.get('/posts', postsController.listPosts)
+routes.put('/posts/:id', postsController.updatePosts)
+routes.delete('/posts/:id', postsController.deletePosts)
 
 //Rotas CRUD Users
+routes.post('/users', usersController.createUsers)
+routes.get('/users', usersController.listUser)
+routes.put('/users/:id', usersController.updateUsers)
+routes.delete('/users/:id', usersController.deleteUsers)
 
+
+//Rotas CRUD Comentarios?
 
 
 module.exports = routes
