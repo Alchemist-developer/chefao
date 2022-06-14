49fd8dc89acfb4664bf238a97b83d77bc0340930
Thais Cavalcante <thaispcavalcante@gmail.com>
2022-06-14 10:57:30 -0300

    teste 2

--- src/Components/FormLogin/index.tsx
+++ src/Components/FormLogin/index.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
+import { Button, Form, Alert } from "react-bootstrap";
+import "./style.css";
+import Logo from "../../assets/logo-colorido.png";
+import { User } from "../../types";
+import { useFormik } from "formik";
+import * as Yup from 'yup';
 
 const FormLogin: React.FC = () => {
   return (
     <div className="background">
-
+        <div className="containerForm">
+        
+      </div>
     </div>
   );
 }
