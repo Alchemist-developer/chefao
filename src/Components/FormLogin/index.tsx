409d728eb9401808e9003cd74397b79c60a1cb4b
Thais Cavalcante <thaispcavalcante@gmail.com>
2022-06-14 18:48:16 -0300

    pagina Login

--- src/Components/FormLogin/index.tsx
+++ src/Components/FormLogin/index.tsx
@@ -5,10 +5,28 @@ import Logo from "../../assets/logo-colorido.png";
 import { User } from "../../types";
 import { useFormik } from "formik";
 import * as Yup from 'yup';
+import { signInUser } from "../../interfaces/users";
+
+const validationSchema = Yup.object({
+  email: Yup.string().email('Por favor preencha com um email válido').required('Por favor preencha com seu email'),
+  password: Yup.string().required('Por favor preencha com uma password').min(8, 'Sua password deve ter no mínimo 8 caracteres')
+})
 
 
 const FormLogin: React.FC = () => {
-    
+  
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    validationSchema,
+    onSubmit: async values => {
+      const {accessToken, user} = await signInUser(values);
+      alert(JSON.stringify({accessToken, nome: user.nome}, null, 2))
+    }
+  })
+
   return (
     <div className="background">
         <div className="containerForm">
@@ -16,12 +34,12 @@ const FormLogin: React.FC = () => {
         <img src={Logo} alt="" />
         </div>
         <h3>LOGIN</h3>
-        <Form className="form">
+        <Form className="form" onSubmit={formik.handleSubmit}>
           <Form.Group className="mb-1">
-            <Form.Control id="email" type="email" placeholder="email"/>
+            <Form.Control id="email" type="email" placeholder="email" value={formik.values.email} onChange={formik.handleChange} isInvalid={formik.touched.email && !!formik.errors.email} isValid={formik.touched.email && !formik.errors.email} />
           </Form.Group>
           <Form.Group className="mb-1" >
-            <Form.Control id="senha" type="password" placeholder="senha"/>
+            <Form.Control id="senha" type="password" placeholder="senha" value={formik.values.password} onChange={formik.handleChange} isInvalid={formik.touched.password && !!formik.errors.password} isValid={formik.touched.password && !formik.errors.password}/>
           </Form.Group>
           <Button variant="primary" type="submit">
             entrar
