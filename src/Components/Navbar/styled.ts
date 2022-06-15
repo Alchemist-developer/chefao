4a1fb56b4f21a547b985bda2e9d3d1e0f25d07bc
Adryel Simon <adryel@live.it>
2022-06-15 00:32:08 -0300

    ajuste navbar link sair e margem navbar

--- src/Components/Navbar/styled.ts
+++ src/Components/Navbar/styled.ts
@@ -12,10 +12,12 @@ export const LinkNavbar = styled.span`
   font-size: 16px;
   color: #6033aa;
   cursor: pointer;
+  display: inline-block;
 `;
 
 export const NavbarComponent = styled(Navbar)`
   box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
   height: 86px;
   padding: 0;
+  margin: 0;
 `;
