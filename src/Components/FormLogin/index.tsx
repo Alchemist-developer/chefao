3efd1820026d43f549852a470a27f5158013c1c1
Thais Cavalcante <thaispcavalcante@gmail.com>
2022-06-15 12:42:23 -0300

    corrigindo bug input senha

--- src/Components/FormLogin/index.tsx
+++ src/Components/FormLogin/index.tsx
@@ -2,14 +2,13 @@ import React from "react";
 import { Button, Form } from "react-bootstrap";
 import "./style.css";
 import Logo from "../../assets/logo-colorido.png";
-import { User } from "../../types";
 import { useFormik } from "formik";
 import * as Yup from 'yup';
 import { signInUser } from "../../interfaces/users";
 
 const validationSchema = Yup.object({
   email: Yup.string().email('Por favor preencha com um email válido').required('Por favor preencha com seu email'),
-  password: Yup.string().required('Por favor preencha com uma password').min(8, 'Sua password deve ter no mínimo 8 caracteres')
+  password: Yup.string().required('Por favor preencha com uma password').min(8, 'Sua senha deve ter no mínimo 8 caracteres').max(12,'Sua senha deve ter no máximo 12 caracteres' )
 })
 
 
@@ -39,7 +38,7 @@ const FormLogin: React.FC = () => {
             <Form.Control id="email" type="email" placeholder="email" value={formik.values.email} onChange={formik.handleChange} isInvalid={formik.touched.email && !!formik.errors.email} isValid={formik.touched.email && !formik.errors.email} />
           </Form.Group>
           <Form.Group className="mb-1" >
-            <Form.Control id="senha" type="password" placeholder="senha" value={formik.values.password} onChange={formik.handleChange} isInvalid={formik.touched.password && !!formik.errors.password} isValid={formik.touched.password && !formik.errors.password}/>
+            <Form.Control id="password" type="password" placeholder="senha" value={formik.values.password} onChange={formik.handleChange} isInvalid={formik.touched.password && !!formik.errors.password} isValid={formik.touched.password && !formik.errors.password}/>
           </Form.Group>
           <Button variant="primary" type="submit" disabled={!formik.isValid}>
             entrar
