6bf6b4f4f20c8a723902778c28eddf9b45bbb72b
Célio Soares <cszenc@gmail.com>
2022-06-16 16:09:08 -0300

    incluídos os validators nas rotas CRUD de Posts, Users e Login

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,21 +4,26 @@ const usersController = require("../domain/Users/controllers/usersController");
 const routes = express.Router();
 
 const authController = require("../domain/Auth/controllers/authController");
-const usersLogin = require("../infrastructure/database/validator/loginTruster");
+const generalLogin = require("../infrastructure/database/validator/loginTruster");
+const updateUser = require("../infrastructure/database/validator/users/updateUser");
+const deleteUser = require("../infrastructure/database/validator/users/deleteUser");
+const updatePosts = require("../infrastructure/database/validator/posts/updatePost");
+const deletePosts = require("../infrastructure/database/validator/posts/deletePost");
+const auth = require("../middlewares/auth")
 
 //Rotas CRUD Posts
 routes.post("/posts/:id", postsController.createPosts);
-routes.get("/posts", postsController.listPosts);
-routes.put("/posts/:id", postsController.updatePosts);
-routes.delete("/posts/:id", postsController.deletePosts);
+routes.get("/posts", auth, postsController.listPosts);
+routes.put("/posts/:id", auth, updateUser, postsController.updatePosts);
+routes.delete("/posts/:id", auth, deleteUser, postsController.deletePosts);
 
 //Rotas CRUD Users
 routes.post("/users", usersController.addUser);
-routes.get("/users", usersController.browseUser);
-routes.put("/users/:id", usersController.updateUser);
-routes.delete("/users/:id", usersController.deleteUser);
+routes.get("/users", auth, usersController.browseUser);
+routes.put("/users/:id", auth, updateUser, usersController.updateUser);
+routes.delete("/users/:id", auth, deleteUser, usersController.deleteUser);
 
 //Rotas CRUD Login
-routes.post("/login", usersLogin, authController.login);
+routes.post("/login", generalLogin, authController.login);
 
 module.exports = routes;
