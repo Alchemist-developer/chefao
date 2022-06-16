be443524a400e12dde1e4fd27c7b4c8e0b1f7cd0
Adryel Simon <adryel@live.it>
2022-06-16 04:09:24 -0300

    icon reposicionado centro divs margin auto

--- src/Components/ProfileData/index.tsx
+++ src/Components/ProfileData/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './style.css'
-import Icon from '/src/assets/icon.png'
+import Icon from '/src/assets/icon.jpeg'
 
 const ProfileData: React.FC = () => {
     return (
