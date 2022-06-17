221fe6d326f405b5f1335025a50e817600ff1069
Thais Cavalcante <thaispcavalcante@gmail.com>
2022-06-17 03:34:40 -0300

    Get de Usuários

--- src/services/users.ts
+++ src/services/users.ts
@@ -5,6 +5,15 @@ export const api = axios.create({
     baseURL: "http://localhost:3333"
 })
 
+export const renderUsers = async () => {
+    try {
+        const response = await api.get("/users")
+        return response.data;
+    } catch(error: any) {
+        alert(`Error: ${error.response.data}`)
+    }
+}
+
 export const createUser = async (user: Omit<User, "id">) => {
     try{
         const response = await api.post("/users", user)
