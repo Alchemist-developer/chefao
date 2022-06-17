93e5420f53e5d66c593dc800ab516ecd6c663c0c
Thais Cavalcante <thaispcavalcante@gmail.com>
2022-06-17 03:35:07 -0300

    atualização ProfileUser

--- src/Pages/ProfileUser/index.tsx
+++ src/Pages/ProfileUser/index.tsx
@@ -5,14 +5,31 @@ import Navbar from '../../Components/Navbar';
 import ProfileData from '../../Components/ProfileData';
 import PostFeed from '../../Components/PostFeed';
 import ProfileFeed from '../../Components/ProfileFeed';
-import { Post } from '../../types';
+import { Post, User } from '../../types';
 import { renderPosts, renderPostsById } from '../../services/posts';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
+import { renderUsers } from '../../services/users';
 
 
 
 function ProfileUser(): JSX.Element {
+
+
+  const [userList, setUserList] = useState<User[]>([] as User[]);
+
+  useEffect(() => {
+
+    const getUsers = async () => {
+        const users = await renderUsers()
+        setUserList(users)
+    }
+    getUsers();
+}, [])
+
+const userById = userList.filter(obj => obj.id == parseInt(window.location.search.split('?')[1]))
+
+console.log(userById)
 
   const [postList, setPostList] = useState<Post[]>([] as Post[]);
 
@@ -27,12 +44,13 @@ function ProfileUser(): JSX.Element {
 
     const postListByID = postList.filter(obj => obj.user_id == parseInt(window.location.search.split('?')[1]))
 
+    console.log(postListByID)
   return (
     <div>
       <Navbar />
       <BodyContainer>
         <FeedContainer>
-          <ProfileData />
+          <ProfileData user={userById} />
           <ProfileFeed posts={postListByID} />
         </FeedContainer>
       </BodyContainer>
