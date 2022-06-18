3af2d50f50d0570fa3821162d253e2750f67b262
Adryel Simon <adryel@live.it>
2022-06-18 12:11:28 -0300

    clean - re-merge

--- Backend/src/infrastructure/database/config/secret.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    chave:"Grupo3HandsOn4"
-}
--- Backend/src/middlewares/auth.js
+++
@@ -1,7 +0,0 @@
-const {expressjwt:expressJWT} = require("express-jwt");
-const secret = require("../infrastructure/database/config/secret")
-
-module.exports = expressJWT({
-    secret:secret.chave,
-    algorithms:["HS256"],
-});
