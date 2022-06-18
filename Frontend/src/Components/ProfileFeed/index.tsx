8f51115f3f7375156741528826837711227a4a37
Thais Cavalcante <thaispcavalcante@gmail.com>
2022-06-18 13:14:15 -0300

    ProfileFeed - Perfil com comentários e sem comentários

--- Frontend/src/Components/ProfileFeed/index.tsx
+++ Frontend/src/Components/ProfileFeed/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import { Post } from '../../types';
-import Icon from "/src/assets/icon.png";
+import Icon from "/src/assets/icon-user.png";
+import Logo from "/src/assets/logo.png";
 import "./style.css"
 import { useSelector } from "react-redux";
 import { RootState } from "../../store";
@@ -14,22 +15,33 @@ interface ProfileFeedProps {
 
 const ProfileFeed: React.FC<ProfileFeedProps> = ({posts}) => {
  
-  return (
-    <Container className='p-0'>
-      {posts.map((post) => (
-        <div className="containerPostFeed">
-          <div className="postFeed">
-            <img src={Icon} alt="" />
-            <div>
-              <h5 className='nomeApartamento'>{post.user_nome} - apê {post.user_apartamento}</h5>
-              <h5 className='dataHora'>13/06/2022 03:05</h5>
-              <p className='comentario'>{post.comentario}</p>
+  if(posts.length === 0) {
+    return (
+      <Container className='p-0'>
+          <h4 className='text-center m-5'>Você ainda não fez nenhuma publicação :(</h4>
+          <div className='text-center'>
+          <img src={Logo} alt="Imagem logo" />
+          </div>
+          </Container>
+    )
+  } else {
+    return (
+      <Container className='p-0'>
+        {posts.map(post => (
+          <div className="containerPostFeed">
+            <div className="postFeed">
+              <img src={Icon} alt="imagem do usuario" />
+              <div>
+                <h5 className='nomeApartamento'>{post.user_nome} - apê {post.user_apartamento}</h5>
+                <h5 className='dataHora'>16/06/2022 19:10</h5>
+                <p className='comentario'>{post.comentario}</p>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-    </Container>
-  );
+        )).reverse()}
+      </Container>
+    );  
+  }
 }
 
 export default ProfileFeed;
