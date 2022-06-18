20da8d9d962c2166c964a09f70830a752be73990
Thais Cavalcante <thaispcavalcante@gmail.com>
2022-06-18 13:05:57 -0300

    atualização imagem e texto componente PostFeed

--- Frontend/src/Components/PostFeed/index.tsx
+++ Frontend/src/Components/PostFeed/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import { Post } from '../../types';
-import Icon from "/src/assets/icon.jpeg";
+import Icon from "/src/assets/icon-user.png";
 import "./style.css"
 import { useSelector } from "react-redux";
 import { RootState } from "../../store";
@@ -23,7 +23,7 @@ const PostFeed: React.FC<PostFeedProps> = ({ posts }) => {
             <img src={Icon} alt="imagem do usuario" />
             <div>
               <h5 className='nomeApartamento'>{post.user_nome} - apê {post.user_apartamento}</h5>
-              <h5 className='dataHora'>13/06/2022 03:05</h5>
+              <h5 className='dataHora'>16/06/2022 19:10</h5>
               <p className='comentario'>{post.comentario}</p>
             </div>
           </div>
